a4c82ba29f09304147d896b34b620fb19c90131b
wdixon <wdixon@bizstream.com>
2023-06-04 16:08:04 -0400

    init website commit with bzs favicon

---
+++ bzs-chess/pages/home.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Box, Center, ChakraProvider, Heading } from "@chakra-ui/react";
+
+export function Home({ Component, pageProps }) {
+  return (
+    <Center>
+      <Heading>It's Time to Play a Game</Heading>
+    </Center>
+  );
+}
+
+export default Home;
