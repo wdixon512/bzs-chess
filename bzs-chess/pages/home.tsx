222a3ca11af8478f3cb97f4347e1d06be2efb9c9
wdixon-bzs <wdixon@bizstream.com>
2023-06-05 08:26:05 -0400

    Updated chess logo color scheme
    added Light/Dark Toggle to Home page

--- bzs-chess/pages/home.tsx
+++ bzs-chess/pages/home.tsx
@@ -2,16 +2,23 @@
 
 import {
   Box,
+  Button,
   Center,
   ChakraProvider,
   Flex,
   Heading,
   Img,
+  useColorMode,
 } from "@chakra-ui/react";
 
 export function Home({ Component, pageProps }) {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Flex direction={"column"}>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
       <Center>
         <Img src="/bzs-chess.png"></Img>
       </Center>
